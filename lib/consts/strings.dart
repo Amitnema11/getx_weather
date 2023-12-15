6e4897f722abbe9d1fdf18da43acf35da732191f
Amit nema <119737697+Amitnema11@users.noreply.github.com>
2023-12-15 16:11:58 +0530

    uploaded

---
+++ lib/consts/strings.dart
@@ -0,0 +1,13 @@
+const String degree = "°";
+const String apiKey = "9ddb6db5718f4aa22b852c2787d8a1e1";
+const String currentEndpoint =
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint =
+    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+// const String latitude = "23.276298764357094";
+// const String longitude = "77.3207233946204";
+
+// const String latitude = "31.577332459776716";
+// const String longitude = "74.1406656310937";
+
+
